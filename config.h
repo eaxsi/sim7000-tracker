44bf81db1d7d43e169b9a93193e0c4185dbb45fc
eaxsi <eero.silfverberg@gmail.com>
2024-02-25 21:24:04 +0200

    Add config file

---
+++ config.h
@@ -0,0 +1,13 @@
+#define PROJECT_NAME "tracker" // Project name, is used in MQTT topic
+
+#define BROKER_HOST "" // IP or address of the MQTT-broker
+#define BROKER_PORT 1883 // MQTT-broker port, MQTT default port is 1883
+#define BROKER_USER "" // MQTT-broker username
+#define BROKER_PASSWD "" // MQTT-broker password
+
+#define GSM_PIN "" // PIN-code of the SIM-card, if no PIN-code leave empty ""
+#define APN "iot.1nce.net" // Carrier APN for internet, 1NCE: iot.1nce.net, Elisa/Telia/DNA: internet
+#define GPRS_USER "" // Username for the cellular internet connection
+#define GPRS_PASS "" // Password for the cellular internet connection
+
+#define HTTP_OTA_URL "" // Address to the the OTA-update file
